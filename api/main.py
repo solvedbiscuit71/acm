105bb688f0ec2b8ef437104decdb7c2769499afe
Praveen Perumal <ppraveen98841@gmail.com>
2023-06-01 23:37:38 +0530

    feat: create /menu route
    GET /menu
    response with list of items fetched from the database

--- api/main.py
+++ api/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from motor.motor_asyncio import AsyncIOMotorClient
+from schema.item import Item
+
+client = AsyncIOMotorClient()
+acm = client['acm']
+
 app = FastAPI()
 
 
 @app.get("/menu")
-def menu():
-    return {"menu": [], "count": 0}
+async def menu() -> list[Item]:
+
+    items = []
+    async for item in acm.items.find():
+        items.append(item)
+
+    return items
 
 
 app.mount('/image', StaticFiles(directory="image"), name="image")
