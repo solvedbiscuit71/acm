514e07484841d1548557e45e3b492e5fe2cf29cf
Praveen Perumal <ppraveen98841@gmail.com>
2023-06-08 11:53:37 +0530

    feat: add token generation

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -9,7 +9,8 @@
         name: string;
     }
 
-    let haveAccount = false
+    let token: string | null = null;
+    let haveAccount: boolean = false;
     let user: User = {
         id: "",
         mobile: "",
@@ -17,20 +18,47 @@
         name: "",
     };
 
-    async function getId(options: any): Promise<string | undefined> {
-        let result = await fetch("http://localhost:8000/user", options);
-        let data = await result.json();
+    async function getId(): Promise<string | undefined> {
+        const options = {
+            method: "GET",
+            headers: {
+                Mobile: user.mobile,
+                Password: user.password,
+            },
+        }
+        const result = await fetch("http://localhost:8000/user", options);
+        const data = await result.json();
 
         switch (result.status) {
             case 200:
-                haveAccount = true
+                haveAccount = true;
+                token = await getToken(data["_id"]) || token
                 return data["_id"];
             case 401:
                 alert("incorrect password");
                 return undefined;
             default:
-                alert("unhandled error occured")
+                alert("unhandled error occured");
         }
+    }
+
+    async function getToken(id: string): Promise<string | undefined> {
+        const payload = {
+            "_id": id,
+            "password": user.password
+        }
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload)
+        };
+
+        const result = await fetch('http://localhost:8000/user/token', options)
+        const data = await result.json()
+        
+        return data["access_token"]
     }
 
     async function createUser() {
@@ -56,23 +84,22 @@
             body: JSON.stringify(payload),
         };
 
-        let result = await fetch("http://localhost:8000/user", options);
-        let data = await result.json();
+        const result = await fetch("http://localhost:8000/user", options);
+        const data = await result.json();
 
         switch (result.status) {
             case 200:
                 user.id = data["_id"];
                 break;
             case 409:
-                user.id = await getId({
-                    method: "GET",
-                    headers: {
-                        Mobile: user.mobile,
-                        Password: user.password,
-                    },
-                }) || user.id;
+                user.id = (await getId()) || user.id;
                 break;
         }
+    }
+
+    $: if (token) {
+        localStorage.setItem("token",token)
+        window.location.replace("/")
     }
 </script>
 
@@ -82,26 +109,38 @@
 
 <main>
     <section>
-    <h1 style:margin-top="{(user.id && !haveAccount) ? '2.625em' : '1.3125em'}">ACM</h1>
+        <h1 style:margin-top={user.id && !haveAccount ? "2.625em" : "1.3125em"}>
+            ACM
+        </h1>
 
-    {#if user.id && !haveAccount}
-        <form class="update-name">
-            <p>
-                <span>What should we</span>
-                <span>call you?</span>
-            </p>
+        {#if user.id && !haveAccount}
+            <form class="update-name">
+                <p>
+                    <span>What should we</span>
+                    <span>call you?</span>
+                </p>
 
-            <Input name="Name" type="text" bind:value={user.name} nolabel placeHolder="Name" />
-            <Button style="align-self: end; font-size: 1.5rem;">></Button>
-        </form>
-    {:else}
-        <form class="create-user" on:submit|preventDefault={createUser}>
-            <Input name="Mobile" type="text" bind:value={user.mobile} />
-            <Input name="Password" type="password" bind:value={user.password} />
+                <Input
+                    name="Name"
+                    type="text"
+                    bind:value={user.name}
+                    nolabel
+                    placeHolder="Name"
+                />
+                <Button style="align-self: end; font-size: 1.5rem;">></Button>
+            </form>
+        {:else}
+            <form class="create-user" on:submit|preventDefault={createUser}>
+                <Input name="Mobile" type="text" bind:value={user.mobile} />
+                <Input
+                    name="Password"
+                    type="password"
+                    bind:value={user.password}
+                />
 
-            <Button style="align-self: center;">Login</Button>
-        </form>
-    {/if}
+                <Button style="align-self: center;">Login</Button>
+            </form>
+        {/if}
     </section>
 </main>
 
