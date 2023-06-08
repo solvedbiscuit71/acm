8c666eb49d300165c86ecda17007ff097cee9314
Praveen Perumal <ppraveen98841@gmail.com>
2023-06-08 09:02:39 +0530

    feat!: initial commit (frontend)

---
+++ frontend/src/lib/Loader.svelte
@@ -0,0 +1,21 @@
+<div class="loader"></div>
+
+<style>
+    div {
+        width: 90px;
+        height: 90px;
+
+        border-radius: 50%;
+        border: 15px solid #F3B07C;
+        border-top-color: #FEF7F0;
+
+        background-color: #FD8736;
+
+        animation: spin 1s linear 0s infinite normal;
+    }
+
+    @keyframes spin {
+        from {transform: rotate(0deg);}
+        to {transform: rotate(360deg);}
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<svelte:head>
+    <title>ACM | Amrita Canteen Management</title>
+</svelte:head>
+
+<script>
+    import Loader from "$lib/Loader.svelte";
+</script>
+
+<main>
+    <section>
+    <h1>ACM</h1>
+
+    <p>
+        <span>Amrita Canteen</span>
+        <span>Management</span>
+    </p>
+
+    <div>
+        <Loader/>
+    </div>
+    </section>
+</main>
+
+<style>
+    main {
+        background-color: #FD8736;
+
+        width: 100vw;
+        height: 100vh;
+    }
+
+    section {
+        position: relative;
+        top: 40%;
+        transform: translateY(-50%);
+    }
+
+    h1, p {
+        text-align: center;
+
+        color: #FFF9F5;
+    }
+
+    h1 {
+        margin: 0 0 0.375em;
+
+        font-size: 4rem;
+    }
+
+    p {
+        margin: 0 0 1.18em;
+        font-size: 2rem;
+        font-weight: 600;
+    }
+
+    span {
+        display: block;
+
+        margin: 0;
+    }
+
+    span:last-of-type {
+        transform: translateY(-0.25em);
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+    }
+</style>
