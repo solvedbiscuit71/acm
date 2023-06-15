1534b91eaca85a9a2d00f425fa58e108845ce573
Praveen Perumal <ppraveen98841@gmail.com>
2023-06-15 17:21:22 +0530

    new: add Navbar component for waiter ui

---
+++ frontend/src/lib/waiter/Navbar.svelte
@@ -0,0 +1,49 @@
+<nav>
+    <ul>
+        <li class="active">Orders</li>
+        <li class="divider"></li>
+        <li>Items</li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #FD8736;
+        min-height: 82px;
+
+        padding: 1em 0em;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    li {
+        font-size: 1.25rem;
+        font-weight: 600;
+
+        color: #ffffff59;
+    }
+
+    li.active {
+        color: white;
+    }
+
+    li:not(.divider) {
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    li.divider {
+        border: 1.5px solid #ffffff80;
+        border-radius: 8px;
+        min-height: 50px;
+        width: 0;
+    }
+</style>
--- frontend/src/routes/waiter/order/+page.svelte
+++ frontend/src/routes/waiter/order/+page.svelte
@@ -1,3 +1,11 @@
 <svelte:head>
     <title>ACM | Amrita Canteen Management | Order</title>
 </svelte:head>
+
+<script lang="ts">
+    import Navbar from "$lib/waiter/Navbar.svelte";
+
+
+</script>
+
+<Navbar/>
